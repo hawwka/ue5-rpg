c9905f4bff3948cc279b0cabb2799e016972ff7f
hawwka <poiuminaj@gmail.com>
2025-05-15 03:56:04 +0400

    Resolved all issues after upgrading editor

--- Source/Slasher/Slasher.h
+++ Source/Slasher/Slasher.h
@@ -1,6 +1,6 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
+#include "DrawDebugHelpers.h"
 
+#define DRAW_SPHERE(Location) if (GetWorld()) DrawDebugSphere(GetWorld(), Location, 15, 20, FColor::Red, true);
